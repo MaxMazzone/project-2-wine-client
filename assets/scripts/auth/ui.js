2e1dd799ef8ecd47efdf0d6ca60509f1f18cdac4
stgeorge3 <amaxmazzone@gmail.com>
2017-09-24 14:30:47 -0400

    Add sign-in function -adds sign in event listener and function -adds sign in ajax call -adds sign in UI handling

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,13 +1,28 @@
+const store = require('../store')
+
 const signUpSuccess = function (data) {
   console.log(data)
   console.log('Successfully Signed Up!')
+  $('#sign-up').trigger('reset')
 }
 
 const signUpFailure = function () {
   console.error('error signing up')
 }
+const signInFailure = function () {
+  console.error('error signing in')
+  $('#message').show()
+}
+const signInSuccess = function (data) {
+  console.log(data)
+  console.log('Successfully Signed in, YO!')
+  store.user = data.user
+  $('#sign-in').trigger('reset')
+}
 
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInFailure,
+  signInSuccess
 }
